61f5c0bcf5b26fca0415561a761bb62b90ffd613
Iremar Satiro <iremar_satiro@hotmail.com>
2021-08-07 20:44:55 -0300

    Serviço temporário e envio de dados do filme

---
+++ src/app/services/dados.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DadosService {
+
+   private dados: any = [];
+
+
+
+  constructor() { }
+
+   guardarDados(index: string, dados: any){
+
+    if(index){
+       this.dados[index] = dados;
+       return true;
+
+      } else{
+
+          return false;
+
+      }
+
+   }
+
+    pegarDados(index: string): any{
+
+        if(index){
+          return this.dados[index];
+
+        }else{
+          return null;
+
+        }
+    }
+
+    deletarDados(index: string): boolean{
+       return delete this.dados[index];
+
+    }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,7 +1,9 @@
+import { DadosService } from './../services/dados.service';
 import { IFilme } from './../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -37,14 +39,21 @@ export class Tab1Page {
       pagina: '/liga-justica'
     }
 
-
-
-
-
   ];
 
 
-  constructor(public alertController: AlertController, public toastController: ToastController) {  }
+  constructor(
+    public alertController: AlertController,
+    public toastController: ToastController,
+    public dadosService: DadosService,
+    public route: Router) {  }
+
+    exibirFilme(filme: IFilme){
+     this.dadosService.guardarDados('filme', filme);
+     this.route.navigateByUrl('/dados-filme');
+     
+
+    }
 
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
