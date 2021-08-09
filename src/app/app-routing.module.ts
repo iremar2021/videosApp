94bca875efa83bf0abd3db95804cbec7415d7893
Iremar Satiro <iremar_satiro@hotmail.com>
2021-08-08 21:51:29 -0300

    Criamos a pagina de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,25 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+
+   {
+      path: '',
+      redirectTo: 'login',
+      pathMatch: 'full'
+   },
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
-  {
-    path: 'mortal-kombate',
-    loadChildren: () => import('./filmes/mortal-kombate/mortal-kombate.module').then( m => m.MortalKombatePageModule)
-  },
-  {
-    path: 'liga-justica',
-    loadChildren: () => import('./filmes/liga-justica/liga-justica.module').then( m => m.LigaJusticaPageModule)
-  },
+
+
   {
     path: 'dados-filme',
     loadChildren: () => import('./dados-filme/dados-filme.module').then( m => m.DadosFilmePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
 ];
 @NgModule({
